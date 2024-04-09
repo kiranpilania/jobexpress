a1bb58a6150d5de5cde3dfc4a94e8b0569eaebc7
kiranpilania <kiranpilania4@gmail.com>
2024-04-09 11:23:13 +0530

    ab

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
 const hbs = require('hbs');
 const app = express();
+
+
 
 const port = process.env.PORT||3000;
 
 const publicDirectoryPath = path.join(__dirname, '/public');
 const viewPath = path.join(__dirname, '/template/views');
 const partialPath = path.join(__dirname, '/template/partials');
+
+
+app.use(bodyParser.json());
 
 app.set('view engine', 'hbs');
 app.set('views', viewPath);
@@ -27,6 +33,7 @@ const job_dataNotes = function () {
 };
 
 
+
 app.get('', (req, res) =>
 {
     res.render('index',{
@@ -35,10 +42,6 @@ app.get('', (req, res) =>
     })
 })
 
-
-
-
-
 app.get('/showjobdetails', (req, res) =>
 {
     fs.readFile('data.json', (err, data) => {
@@ -46,9 +49,71 @@ app.get('/showjobdetails', (req, res) =>
         const jsonData = JSON.parse(data);
         res.render('showjobdetails', { jsonData: jsonData, title: 'Job_Hunt' });
     });
-
-    
 })
+
+
+
+app.get('/about', (req, res) =>
+{
+    res.render('about',{
+        title: 'Job Hunt',
+    })
+})
+
+app.get('/topjobs', (req, res) =>
+{
+    res.render('topjobs',{
+        title: 'Job Hunt',
+    })
+})
+
+
+app.post('/applyJob', (req, res) => {
+    const dataToSave = req.body;
+    
+    // Write data to JSON file
+    fs.readFile('job_applications.json', 'utf8', (err, data) => {
+       if (err) {
+          console.error(err);
+          res.status(500).send('Error saving data');
+          return;
+       }
+       
+       let jobApplications = [];
+       if (data) {
+          jobApplications = JSON.parse(data);
+       }
+       
+       jobApplications.push(dataToSave);
+ 
+       fs.writeFile('job_applications.json', JSON.stringify(jobApplications, null, 2), err => {
+          if (err) {
+             console.error(err);
+             res.status(500).send('Error saving data');
+             return;
+          }
+          console.log('Data saved successfully');
+          res.send('Data saved successfully');
+       });
+    });
+ });
+
+
+ 
+
+ app.get('/appliedjob', (req, res) =>
+ {
+     fs.readFile('job_applications.json', (err, job_applications) => {
+         if (err) throw err;
+         const jsonData = JSON.parse(job_applications);
+         res.render('appliedjob', { jsonData: jsonData, name: '' });
+     });
+ })
+
+
+
+
+
 
 
 
